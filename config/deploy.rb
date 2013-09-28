9177fef61b9cc551b0257e7113c2862aec46fe54
thattommyhall <thattommyhall@gmail.com>
2013-09-28 02:00:06 +0100

    Add lein dep

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,4 +1,4 @@
-set :application, "hugo-a-go-gp"
+set :application, "hugo-a-go-go"
 set :repository,  "deveo@deveo.com:clojurecup/projects/hugoagogo/repositories/git/hugo-a-go-go"
 set :repository_cache,    "#{application}_cache"
 set :environment,         "production"
@@ -7,7 +7,7 @@ set :user,                "root"
 set :scm_user,            "root"
 set :deploy_to,           "/var/www/#{application}"
 set :keep_releases,       2
-set :deploy_via,          :remote_cache
+#set :deploy_via,          :remote_cache
 set :scm,                 :git
 set :use_sudo, false
 set :ssh_options, {:forward_agent => true}
@@ -19,11 +19,12 @@ after 'deploy:setup', :custom_setup
 task :custom_setup do
   run "apt-get update"
   run "apt-get install -y --force-yes git build-essential openjdk-7-jre-headless"
+  put File.read(File.join(File.dirname(__FILE__), 'lein')), '/usr/bin/lein', :mode => '711'
 end
 
 task :update_upstart do
   put File.read(File.join(File.dirname(__FILE__), 'upstart')), '/tmp/upstart', :mode => '644'
-  sudo "mv -f /tmp/upstart /etc/init/ca.conf"
+  sudo "mv -f /tmp/upstart /etc/init/hugo.conf"
 end
 
 task :update_ssh do
@@ -31,8 +32,7 @@ task :update_ssh do
   sudo "mv -f /tmp/authorized_keys /root/.ssh/authorized_keys"
 end
 
-before :deploy, :update_ssh
-before "deploy:restart", :update_upstart
+before :deploy, :update_ssh, :update_upstart
 after "deploy:restart", "deploy:cleanup"
 
 namespace :deploy do
